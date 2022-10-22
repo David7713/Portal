1f94b07ccfee2039645a5d5349cc0dd04702b756
David7713 <101974492+David7713@users.noreply.github.com>
2022-10-22 17:58:04 +0400

    addSpinner

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import { useState, useEffect } from 'react'
+import CircleLoader from "react-spinners/CircleLoader";
+import "./styles/Spinner.css"
 const App = () => {
+  const [loading,setLoading] = useState(false)
+  useEffect(()=>{
+    setLoading(true)
+    setTimeout(()=>{
+setLoading(false)
+    },3000)
+ },[])
   return (
-    <div>Hello All</div>
+    <div className='App'>
+
+{/* spinner */}
+{
+    loading?
+    <CircleLoader
+    className='Spinner'
+    color="hsla(195, 67%, 53%, 1)"
+    cssOverride={{}}
+    size={160}
+    speedMultiplier={1}
+  />
+    :
+    <div > 
+    <p>Portal Project Incoming</p>
+
+  
+</div>
+}
+    </div>
+
   )
 }
 
