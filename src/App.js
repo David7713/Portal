573ad14121af6f34577d6d52c2f9ea3d0a6b147d
David7713 <101974492+David7713@users.noreply.github.com>
2022-10-30 23:10:29 +0400

    addLanguage3

--- src/App.js
+++ src/App.js
@@ -72,11 +72,8 @@ setLoading(false)
 <div className='a'>
   <div className='logo'></div>
 <select className='selectflags'  name='language' onChange={onChange}>
-<<<<<<< HEAD
-<option value="en"> English1</option>
-=======
-<option value="en">Englich </option>
->>>>>>> 2df5fc4a84cb51a2715585a9f7655f957b92dc32
+<option value="en">English </option>
+
 
 <option value="fr">French1</option>
 
