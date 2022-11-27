0d30843ea2b68b3934750785962daf42d3f0ae60
David7713 <101974492+David7713@users.noreply.github.com>
2022-11-27 15:00:25 +0400

    Done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,7 +21,7 @@ const Contact = () => {
 <div className='contact-part-2'>
     <p id='contact2-links'>{t('Links')} </p>
     <br></br>
-      <a className='contact-links-2'   href='/Portal'>{t('Home')}</a>
+      <a className='contact-links-2'   href='/Portal/'>{t('Home')}</a>
       <a className='contact-links-2'   href='/Portal/miami'>{t('Miami')}</a>
       <a className='contact-links-2'   href='/Portal/newyork'>{t('Newyork')}</a>
       <a className='contact-links-2'   href='/Portal/baltimore'>{t('Baltimore')}</a>
